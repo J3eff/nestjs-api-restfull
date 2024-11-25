6984538a7d0b05801c6e82a0bbb421d726c92c76
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-25 01:00:06 -0300

    Validando e-mail único

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true, //Lança um erro se mandar um dado no json que não esta no DTO
     }),
   );
+
+  //Resolve as injeção de dependencia do class-validator
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
