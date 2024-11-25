93df5fa43d8ac9328a8a300cf18c71dac7b5d492
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-25 15:39:11 -0300

    Configurando o TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostegresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostegresConfigService,
+      inject: [PostegresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
