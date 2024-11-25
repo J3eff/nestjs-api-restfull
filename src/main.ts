092111ddbd3cf471a72fe3d8b25bd8f6836a154c
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-25 00:05:24 -0300

    Validando o usuário

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true, //Quando nest for usar a validação deste pipe ele vai ignorar todas propriedades que não tiverem no DTO
+      forbidNonWhitelisted: true, //Lança um erro se mandar um dado no json que não esta no DTO
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
