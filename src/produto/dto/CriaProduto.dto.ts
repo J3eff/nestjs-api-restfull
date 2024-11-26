9340e853c00e80c71ba96cbd5215ff0182443a75
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-26 13:47:27 -0300

    Usando a CLI do TypeORM

--- src/produto/dto/CriaProduto.dto.ts
+++ src/produto/dto/CriaProduto.dto.ts
@@ -6,7 +6,6 @@ import {
   IsNumber,
   IsString,
   IsUrl,
-  IsUUID,
   MaxLength,
   Min,
   ValidateNested,
@@ -40,8 +39,8 @@ export class ImagemProdutoDTO {
 }
 
 export class CriaProdutoDTO {
-  @IsUUID(undefined, { message: 'ID de usuário inválido' })
-  usuarioId: string;
+  // @IsUUID(undefined, { message: 'ID de usuário inválido' })
+  // usuarioId: string;
 
   @IsString()
   @IsNotEmpty({ message: 'Nome do produto não pode ser vazio' })
