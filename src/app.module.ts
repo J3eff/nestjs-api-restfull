f9634c9d46358a529712fe13a1a2a06b17953385
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-28 01:53:27 -0300

    organização das pastas do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { ProdutoModule } from './produto/produto.module';
-import { UsuarioModule } from './usuario/usuario.module';
+import { UsuarioModule } from './modulos/usuario/usuario.module';
+import { ProdutoModule } from './modulos/produto/produto.module';
 import { PostgresConfigService } from './config/postgres.config.service';
-import { PedidoModule } from './pedido/pedido.module';
+import { PedidoModule } from './modulos/pedido/pedido.module';
 import { APP_FILTER } from '@nestjs/core';
-import { FiltroDeExecaoGlobal } from './filtros/filtro-de-execao-global';
+import { FiltroDeExecaoGlobal } from './recursos/filtros/filtro-de-execao-global';
 
 @Module({
   imports: [
