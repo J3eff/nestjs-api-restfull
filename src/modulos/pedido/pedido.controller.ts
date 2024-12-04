35f318c05ce83186bd6d54f99a730fb4cdece1e9
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-04 15:20:42 -0300

    Usando dados do usuário

--- src/modulos/pedido/pedido.controller.ts
+++ src/modulos/pedido/pedido.controller.ts
@@ -6,12 +6,16 @@ import {
   Patch,
   Post,
   Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { PedidoService } from './pedido.service';
 import { CriaPedidoDTO } from './dto/CriaPedido.dto';
 import { AtualizaPedidoDto } from './dto/AtualizaPedido.dto';
-import { AutenticacaoGuard } from '../autenticacao/autenticacao/autenticacao.guard';
+import {
+  AutenticacaoGuard,
+  RequisicaoComUsuario,
+} from '../autenticacao/autenticacao/autenticacao.guard';
 
 @UseGuards(AutenticacaoGuard)
 @Controller('pedidos')
@@ -20,9 +24,10 @@ export class PedidoController {
 
   @Post()
   async criaPedido(
-    @Query('usuarioId') usuarioId: string,
+    @Req() req: RequisicaoComUsuario,
     @Body() dadosDoPedido: CriaPedidoDTO,
   ) {
+    const usuarioId = req.usuario.sub;
     const pedidoCriado = await this.pedidoService.cadastraPedido(
       usuarioId,
       dadosDoPedido,
@@ -34,7 +39,8 @@ export class PedidoController {
   }
 
   @Get()
-  async obtemPedidosDeUsuario(@Query('usuarioId') usuarioId: string) {
+  async obtemPedidosDeUsuario(@Req() req: RequisicaoComUsuario) {
+    const usuarioId = req.usuario.sub;
     const pedidos = await this.pedidoService.obtemPedidosDeUsuario(usuarioId);
 
     return pedidos;
